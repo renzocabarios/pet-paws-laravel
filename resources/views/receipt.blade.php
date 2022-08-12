f3b8ed6c019f94cb705465909abb0cad1dda5472
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-12 20:02:57 +0800

    Fix typo in receipt view

--- resources/views/receipt.blade.php
+++ resources/views/receipt.blade.php
@@ -6,8 +6,8 @@
 
     <div class=" items-center shadow-lg shadow-gray-900 bg-white rounded-md p-12">
         <div class="text-3xl grid font-bold grid-cols-2 gap-20">YOUR RECEIPT <button type="submit" class="bg-sky-300 rounded-md justify-center text-white">Excell</button></div>
-       <div class="space-x-24"> 
-<br>
+        <div class="space-x-24">
+            <br>
             <div class="grid font-bold grid-cols-2 gap-20">
                 <label>Summer Cut</label> <span> $500</span>
             </div>
@@ -17,16 +17,13 @@
             <div class="grid font-bold grid-cols-2 gap-20">
                 <label>Halloween Cut</label> <span> $500</span>
             </div>
-    <br>                
+            <br>
         </div>
-                    
-          
-           
-            <div class="justify-center">
-                        <button type="submit" class="bg-lime-500 rounded-md w-full flex justify-center  py-1 text-white">CHECK OUT</button>
-                    </div>
-                </div>
-     </div>
+        <div class="justify-center">
+            <button type="submit" class="bg-lime-500 rounded-md w-full flex justify-center  py-1 text-white">Go Back</button>
+        </div>
     </div>
 </div>
-        @endsection()
+</div>
+</div>
+@endsection()
