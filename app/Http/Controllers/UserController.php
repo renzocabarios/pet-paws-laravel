8b67871e033d064c5320377fa0086ec576cb3e34
Renzo Cabarios <renzo.cabarios@gmail.com>
2022-08-12 13:33:44 +0800

    Fix signin

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,8 +10,9 @@ class UserController extends Controller
 {
     public function authUser(Request $request)
     {
-        $user = User::where('email', $request->email)->andWhere('active', true)->first();
-        if ($user == null) return redirect('/');
+
+        $user = User::where(['email' => $request->email, 'active' => true])->first();
+        if ($user == null) return redirect('/fail');
         if (!(Hash::check($request->password, $user->password))) return redirect('/fail');
         auth()->login($user);
         if ($user->role == "Admin" || $user->role == "Employee") return redirect('/pet');
